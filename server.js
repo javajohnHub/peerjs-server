d1779a21fe4d52f2484d75cdc52afc6546484e7c
javajohnhub <joedw1978@gmail.com>
2017-11-11 15:41:54 -0500

    w45y

--- server.js
+++ server.js
@@ -1,2 +1,2 @@
 var PeerServer = require('peer').PeerServer;
-var server = PeerServer({port: 9000, path: '/peerjs', secure: true});
+var server = PeerServer({port: 80, path: '/peerjs', secure: true});
